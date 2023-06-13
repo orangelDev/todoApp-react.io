8e7d96a1d2dd5ffdac199685f0ca76320a877005
Orangel Brito <orangeleando@gmail.com>
2023-06-12 20:55:27 -0500

    rdy to deploy

--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../TodoCounter/TodoCounter.css"
 import { TodoContext } from '../TodoContext';
+import confetti from "canvas-confetti";
 
 function TodoCounter() {
 	const {
@@ -11,14 +12,14 @@ function TodoCounter() {
 	return (
 		
 		totalTodos === completedTodos &&  searchedTodos.length === 1 ?
-
-		<h2 className="title"> Felicidades, completaste todas las tareas crack!!</h2>
+		
+		<h2 className="title"> Congratulations, you have completed all the tasks crack!!</h2>
 
 		:
 
 		<h2 className="title">
 			<span> {completedTodos} </span>
-				Tareas completadas de
+				Task completed of
 			<span> {totalTodos} </span>
 		</h2>
 		
